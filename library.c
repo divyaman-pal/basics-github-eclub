f66c6eb9393df91e1e8305cbfcd85befbb2cb6c8
divyaman pal <divyamp23@iitk.ac.in>
2024-05-11 16:40:57 +0530

    Done

--- library.c
+++ library.c
@@ -7,5 +7,6 @@ int main() {
  printf("the value of a is %d %d \n", a, a);
  printf("the value of b is %f \n", b);
  printf("the value of c is %c", c);
+ printf("My name is Karpurapu")
  return 0;
 }
