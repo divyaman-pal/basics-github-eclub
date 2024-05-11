51a77bf7b748c4f2c11c675b2cfc68fee20ea318
divyaman pal <divyamp23@iitk.ac.in>
2024-05-11 13:45:34 +0530

    just check

---
+++ ps.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("enter the value of a");
+    scanf("%d", &a);
+
+    printf("enter value of b");
+    scanf("%d", &b);
+printf ("area is %d ", a*b);
+return 0;
+}
