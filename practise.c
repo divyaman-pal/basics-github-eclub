857baba5eae3ac0caf1afd2e2a2b9b908d3a8da1
divyaman pal <divyamp23@iitk.ac.in>
2024-05-11 13:41:45 +0530

    checking

---
+++ practise.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+
+int main(){
+        int a ;
+    printf("enter the weight of watermelon ");
+    scanf("%d",&a);
+    if(a%2==0){
+        printf("yes");
+    }
+    else{
+        printf("no");
+    }
+    return 0;
+}
