86e565c0de4ffe3939f7f5bfe4e33c93b035f829
divyaman pal <divyamp23@iitk.ac.in>
2024-05-11 16:35:42 +0530

    First commit

---
+++ library.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+ int a = 4;
+ float b = 9.5;
+ char c = 'u';
+ printf("the value of a is %d %d \n", a, a);
+ printf("the value of b is %f \n", b);
+ printf("the value of c is %c", c);
+ return 0;
+}
